7007b26d34189c3928f65c941ad6205cc5359e4e
chibi-chichi <78858443+chibi-chichi@users.noreply.github.com>
2021-08-28 22:42:46 +0900

    barely started

--- main_system.py
+++ main_system.py
@@ -1,1 +1,14 @@
 import requests
+import sys, os
+
+url = 'https://kauth.kakao.com/oauth/token'
+rest_api_key = ''
+redirect_url = 'https://example.com/oauth'
+authorize_code = ''
+
+data = {
+    'grant_type':'authorize_code'
+    'client_id':rest_api_key
+    'redirect_url':redirect_url
+    'code':authorize_code
+}
